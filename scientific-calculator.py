3703409177a7d4c321610a4d807e819b8426fdd8
Evren Yilmaz Yakin <evrenykn@gmail.com>
2018-02-16 00:45:16 +0300

    test
    test

--- scientific-calculator.py
+++ scientific-calculator.py
@@ -1,2 +1,16 @@
-#test
-print (2 + 2)
+#test_version_01
+
+def add(a,b):
+    return a + b
+
+print('Welcome to Scientific Calculator V 0.0.1')
+print('Please enter the first value')
+
+a=input()
+
+print('Please enter the second value')
+
+b=input()
+
+print('The Result:', a+b)
+
